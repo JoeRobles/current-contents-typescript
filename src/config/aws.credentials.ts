cb45391ddfe87dc6c859d77de03fb8dd794e8469
Joe Robles <joe.robles.pdj@gmail.com>
2019-08-31 14:02:40 -0500

    removing credentials

--- src/config/aws.credentials.ts
+++ src/config/aws.credentials.ts
@@ -1,6 +1,6 @@
 export enum AwsCredentials {
-  ACCESS_KEY_ID = 'AKIARPWHNAHEC23PNTJH',
-  ENDPOINT = 'rds.us-west-2.amazonaws.com',
-  REGION = 'us-west-2',
-  SECRET_KEY = 'J77wUhW6i8ITBLfEVb66J1rIlh64PpJro2jylY4x',
+  ACCESS_KEY_ID = '',
+  ENDPOINT = '',
+  REGION = '',
+  SECRET_KEY = '',
 }
